005ca0dc08dbf4ede6650d51a7d132a53dd1caa4
Tathya-Dixit <77059059+Tathya-Dixit@users.noreply.github.com>
2021-01-21 19:58:24 +0530

    Update caller.py

--- caller.py
+++ caller.py
@@ -1,16 +1,19 @@
 from speak import speak
-from processor import wishme
 from takecommand import takecommand
 from datafile import askme
 from processor import wishme
 
 
 def call():
-    if 1:
-        speak("hi sir, there for you always")
-        wishme()
-        while True:
-            a = takecommand()
-            askme(a)
-    else:
-        call()
+    try:
+        init = takecommand()
+        if init == "alex":
+            speak("hi sir, there for you always")
+            wishme()
+            while True:
+                a = takecommand()
+                askme(a)
+        else:
+            call()
+    except Exception as a:
+        print(a)
