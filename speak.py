ece0e7f9555561d23f642f8b4524a7f7e1a2a77b
Tathya-Dixit <77059059+Tathya-Dixit@users.noreply.github.com>
2021-01-21 20:03:30 +0530

    Update speak.py

--- speak.py
+++ speak.py
@@ -2,11 +2,11 @@ import pyttsx3
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[0].id)
+engine.setProperty("voice", voices[0].id) # for female voice voices[1].id
 
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-    print("jarvis said : ", audio)
+    print(audio)
     
